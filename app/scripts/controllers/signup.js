33cd6887063f85313a00bba3a15ab304bffd3b50
lee byung sik <lbs1123_@naver.com>
2017-10-21 12:57:48 +0900

    WebView 추가

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -8,10 +8,25 @@
  * Controller of the angularJsexamApp
  */
 angular.module('angularJsexamApp')
-  .controller('SignupCtrl', function () {
+  .controller('SignupCtrl', [ 
+  	"Data", "$scope", "$state",
+  	function (Data, $scope, $state) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+    $scope.name = "";
+    $scope.age  = "";
+
+    $scope.saveUserInfo = function() {
+    	var dataPromise = Data.setData(
+    		'http://10.0.2.2:52273/user', '&name='+$scope.name+'&age='+$scope.age);
+    	dataPromise.then(function(results){
+    		$scope.name = "";
+    		$scope.age  = "";
+    	},function(reason){},function(update){});
+    };
+
+  }]);
--- app/scripts/controllers/user-list.js
+++ app/scripts/controllers/user-list.js
@@ -23,11 +23,36 @@ angular.module('angularJsexamApp')
     $scope.userList = [];
     $scope.requestUserList = function() {
     	var dataPromise = Data.getData(
-    		'http://127.0.0.1:52273/user');
+    		'http://10.0.2.2:52273/user');
     	dataPromise.then(function(results) {
     		$scope.userList = results.data;
     	}, function(reason){},function(update){});
 
+    }
+
+    $scope.deleteUserInfo = function(id) {
+    	var dataPromise = Data.deleteData(
+    		'http://10.0.2.2:52273/user/'+id, '');
+    	dataPromise.then(function(results) {
+    		$scope.requestUserList();
+    	}, function(reason){},function(update){});
+    }
+
+    $scope.modifyUserInfo = function(id, name, age) {
+    	var dataPromise = Data.modifyData(
+    		'http://10.0.2.2:52273/user/'+id, '&name='+name+'&age='+age);
+    	dataPromise.then(function(results) {
+    		$scope.requestUserList();
+    	}, function(reason){},function(update){});
+    }
+
+    $scope.userInfo = {};
+    $scope.getUserInfo = function(id) {
+    	var dataPromise = Data.getData(
+    		'http://10.0.2.2:52273/user/'+id);
+    	dataPromise.then(function(results) {
+    		$scope.userInfo = results.data;
+    	}, function(reason){},function(update){});
     }
 
   }]);
